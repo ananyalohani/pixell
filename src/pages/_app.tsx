90c813b5ce2417f80069247ced2c8e78f169fd53
Ananya Lohani <ananya19018@iiitd.ac.in>
2022-01-02 22:04:44 +0530

    feat: Add SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,24 @@ import Footer from "@/components/Footer";
 import { GridContextProvider } from "@/context/GridContext";
 import { DAppProvider } from "@usedapp/core";
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <DAppProvider config={{}}>
       <GridContextProvider>
         <ChakraProvider>
+          <Head>
+            <title>Pixell</title>
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <meta
+              name="description"
+              content="Pixell is a pixel art NFT marketplace on the Ethereum blockchain. Create, mint, sell and buy NFTs on the Ropsten Testnet."
+            />
+          </Head>
           <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex flex-col flex-1">
